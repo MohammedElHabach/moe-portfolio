7bca79760ec0b2d3df812923b70d51f73a1132bd
Mohammed El Habach <mhamadhabach06@gmail.com>
2023-05-09 21:37:44 +0300

    projects crud without multer

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,59 +1,75 @@
-const asyncHandler = require('express-async-handler')
-
+const asyncHandler = require("express-async-handler");
+const Project = require("../models/projectModel");
 // @desc    Get projects
 // @route   GET /api/projects
 // @access  Public
-const getProject = async(req,res) => {
-    res.status(200).json({message:'get projects'}) 
-}
+const getProject = async (req, res) => {
+  const projects = await Project.find();
+  res.status(200).json(projects);
+};
 
 // @desc    Create project
 // @route   GET /api/projects
 // @access  Private
-const createProject = asyncHandler( async(req,res) => {
-    if(!req.body.name){
-        res.status(400)
-        throw new Error('Please add a name field')
-    }if(!req.body.img){
-        res.status(400)
-        throw new Error('Please add a pic')
-    }
-    if(!req.body.desc){
-        res.status(400)
-        throw new Error('Please add a description')
-    }
-    if(!req.body.repoLink){
-        res.status(400)
-        throw new Error('Please add a repo Link')
-    }
-    if(!req.body.demoLink){
-        res.status(400)
-        throw new Error('Please add a demo Link')
-    }
-    res.status(200).json({message:'create projects'}) 
+const createProject = asyncHandler(async (req, res) => {
+  if (!req.body.name) {
+    res.status(400);
+    throw new Error("Please add a name field");
+  }
+  if (!req.body.img) {
+    res.status(400);
+    throw new Error("Please add a pic");
+  }
+  if (!req.body.repoLink) {
+    res.status(400);
+    throw new Error("Please add a repo Link");
+  }
 
-})
+  const project = await Project.create({
+    name: req.body.name,
+    desc: req.body.desc,
+    img: req.body.img,
+    repoLink: req.body.repoLink,
+    demoLink: req.body.demoLink,
+  });
+
+  res.status(200).json(project);
+});
 
 // @desc    Update project
 // @route   PUT /api/projects/:id
 // @access  Private
-const updateProject = asyncHandler( async(req,res) => {
-    res.status(200).json({message:`update project ${req.params.id}`}) 
-})
+const updateProject = asyncHandler(async (req, res) => {
+  const project = await Project.findById(req.params.id);
+  if (!project) {
+    res.status(400);
+    throw new Error("Project Not Found");
+  }
+  const updatedProject = await Project.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedProject);
+});
 
 // @desc    Delete project
 // @route   DELETE /api/projects/:id
 // @access  Private
-const deleteProject = asyncHandler( async(req,res) => {
-    res.status(200).json({message:`delete project ${req.params.id}`}) 
+const deleteProject = asyncHandler(async (req, res) => {
+    const project = await Project.findById(req.params.id);
+    if (!project) {
+      res.status(400);
+      throw new Error("Project Not Found");
+    }
+    await project.deleteOne({_id: req.params.id})
+  res.status(200).json({ id: req.params.id,msg:"Deleted Successfully"});
+});
 
-})
-
-
-
-module.exports= {
-    getProject,
-    createProject,
-    updateProject,
-    deleteProject
-}
+module.exports = {
+  getProject,
+  createProject,
+  updateProject,
+  deleteProject,
+};
---
+++ backend/models/projectModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    desc: {
+      type: String,
+    },
+    img: {
+      type: String,
+      required: [true, "Please add an image"],
+    },
+    repoLink: {
+      type: String,
+      required: [true, "Please add the repo Link"],
+    },
+    demoLink: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Project", projectSchema);
